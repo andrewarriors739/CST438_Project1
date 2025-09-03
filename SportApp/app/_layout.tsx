eeeac32eb981fce54b917882aba8cd6cf8d09840
mari-924 <marianaduran260@gmail.com>
2025-09-02 21:21:30 -0700

    Added Search page for teams, also added a font package to the layout

--- SportApp/app/_layout.tsx
+++ SportApp/app/_layout.tsx
@@ -1,9 +1,30 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { initDatabase } from "@/database/database";
+import { HammersmithOne_400Regular } from '@expo-google-fonts/hammersmith-one';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    HammersmithOne_400Regular,
+  });
+
   useEffect(() => {
     initDatabase();
   }, []);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
