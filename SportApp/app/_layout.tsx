f9d22e8300cc3888e4508488405e47e886299826
SebastianRamos3 <seramos@csumb.edu>
2025-09-10 00:03:58 -0700

    Set up functionality to search up player and get that players information through an api call, and have that information show up on screen.

--- SportApp/app/_layout.tsx
+++ SportApp/app/_layout.tsx
@@ -1,14 +1,13 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { initDatabase } from "@/database/database";
 
-import { HammersmithOne_400Regular } from '@expo-google-fonts/hammersmith-one';
+import { HammersmithOne_400Regular } from "@expo-google-fonts/hammersmith-one";
 
 SplashScreen.preventAutoHideAsync();
-
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -35,16 +34,23 @@ export default function RootLayout() {
         <Drawer.Screen
           name="index"
           options={{
-            drawerLabel: 'Home',
-            title: 'Home',
+            drawerLabel: "Home",
+            title: "Home",
           }}
         />
         <Drawer.Screen
-        name = "profile"
-        options = {{
-          drawerLabel: 'Profile', 
-          title: 'Profile',
-        }}
+          name="profile"
+          options={{
+            drawerLabel: "Profile",
+            title: "Profile",
+          }}
+        />
+        <Drawer.Screen
+          name="players"
+          options={{
+            drawerLabel: "Players",
+            title: "Players",
+          }}
         />
       </Drawer>
     </GestureHandlerRootView>
